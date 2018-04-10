7e5655a23ce53044a3d92174a29e644526d18a5e
M.D <dmytrenko.mykhaylo@gmail.com>
2018-04-10 14:26:05 +0300

    Login test debug

--- src/test/java/com/headcovers/tests/selenium/smoke/LoginTest.java
+++ src/test/java/com/headcovers/tests/selenium/smoke/LoginTest.java
@@ -1,14 +1,22 @@
 package com.headcovers.tests.selenium.smoke;
 
 import com.headcovers.tests.selenium.BaseTest;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LoginTest extends BaseTest{
-    @Test
-    public void LoginRegisteredUser(){
+
+    @Before
+    public void RegUsersForTestData(){
         mainPage.navigatetoSignInPage()
                 .navigatetoRegistrationPage()
-                .registrateNewUser("testfirstname", "testlastname", "blackridgetesterloginTest@gmail.com", "q1w2E#R$", "q1w2E#R$")
-                .logOut();
+                .registrateNewUser("testfirstname", "testlastname", "blackridgetesterloginTest@gmail.com", "q1w2E#R$", "q1w2E#R$");
+    }
+
+    @Test
+    public void LoginValidRegisteredUser(){
+        mainPage.navigatetoSignInPage()
+                .loginAs("blackridgetesterloginTest@gmail.com", "q1w2E#R$")
+                .assertMyDashboardPresented();
     }
 }
