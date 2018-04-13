3d626116e66c67a92046258d3071b9354b26864e
M.D <dmytrenko.mykhaylo@gmail.com>
2018-04-13 15:42:21 +0300

    Swatcher: Find all colors of product on the current page, choose first color

--- src/test/java/com/headcovers/pages/customer/product/ProductWarpPage.java
+++ src/test/java/com/headcovers/pages/customer/product/ProductWarpPage.java
@@ -4,6 +4,12 @@ import com.headcovers.pages.WarpMainPage;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import java.util.List;
 
 public class ProductWarpPage extends WarpMainPage{
     public ProductWarpPage(WebDriver driver){
@@ -14,9 +20,27 @@ public class ProductWarpPage extends WarpMainPage{
     protected By productNameLocator = By.xpath("//*[@itemprop='name']");
     protected By price = By.xpath("//*[@class='price']");
 
+    //attributes, all colors in swatchers
+    protected By colorSwatcherBlock = By.id("color-swatch-attribute-92");
+    @FindAll({
+            @FindBy(how = How.XPATH, using = "//*[@id=\"color-swatch-attribute-92\"]/li")
+    })
+    private List<WebElement> colorsInSwatcher;
+    //protected List<By>
+
     public ProductWarpPage checkThatOpenedProductPageCorrespodToTheWordInSearch(String oneWord, String secondChar, String thirdChar){
         String productName = driver.findElement(productNameLocator).getText();
         Assert.assertTrue(productName.contains(oneWord+secondChar+thirdChar));
+        return this;
+    }
+
+    public ProductWarpPage chooseFirstColorBySwatcher(){
+        colorsInSwatcher.get(1).click();
+        return this;
+    }
+
+    public ProductWarpPage chooseColordBySwatcher(String chooseByText){
+        //implement choosing by text
         return this;
     }
 
